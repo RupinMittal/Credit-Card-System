79b2cd2b4af64bf37b33deff2c4427035ba7e5f3
RupinMittal <42721308+RupinMittal@users.noreply.github.com>
2020-08-20 21:08:53 -0700

    Figured out how to use random -- implemented randomAmount() in Generator

--- CreditCardSystem.h
+++ CreditCardSystem.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-extern struct transaction; //the struct that will represent the transactions
-extern struct transaction record[];
+extern struct transaction;              //the struct that will represent the transactions
+extern struct transaction record[];     //the array of structs that holds all the transactions
 
 #endif
--- RecordGenerator.c
+++ RecordGenerator.c
@@ -1,15 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
+int randomAmount()
+{
+    return (rand() % 2000);     //return an amount from $0 - $2000
+}
 int main()
 {
+    
     FILE *recordPtr;    //the pointer to the record.txt file
 
     recordPtr = fopen("Record.txt", "w");
 
     if(recordPtr != NULL)
     {
-        fprintf(recordPtr, "YEET, It worked!");
+        srand(time(0));             //set the seed based on the time since Jan 1, 1970 (Unix timestamp)
+        for(int i = 0; i < 10; i++)
+            fprintf(recordPtr, "%d\n", randomAmount());
     }
     else
     {
