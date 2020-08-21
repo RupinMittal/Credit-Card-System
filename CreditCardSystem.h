d19464d3adc46f9548bfabd3ba23d5c30c1e3aa4
RupinMittal <42721308+RupinMittal@users.noreply.github.com>
2020-08-20 19:28:02 -0700

    wrote CreditCardSystem.h header file with struct and struct array

---
+++ CreditCardSystem.h
@@ -0,0 +1,10 @@
+#ifndef CREDITCARDSYSTEM_H  //guard against infinite loop of includes
+#define CREDITCARDSYSTEM_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+extern struct transaction; //the struct that will represent the transactions
+extern struct transaction record[];
+
+#endif
