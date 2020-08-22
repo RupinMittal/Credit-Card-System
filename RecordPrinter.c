71db2eb1fe1a7aaf48bece0ee362b3395021eda5
RupinMittal <42721308+RupinMittal@users.noreply.github.com>
2020-08-21 20:37:12 -0700

    Added RecordPrinter.c file to print record. Wrote printtofile method (tested-works)

---
+++ RecordPrinter.c
@@ -0,0 +1,37 @@
+/**
+ * \class RecordPrinter.c
+ * This class prints the list of Transactions
+ * \author $Rupin Mittal $
+ * \date $Date: 8/21/20
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "CreditCardSystem.h"
+#include "RecordHeader.h"
+
+//prints the record out to the file Record.txt
+void printRecordToFile()
+{
+    FILE *recordPtr;                        //the pointer to the record.txt file
+    
+    recordPtr = fopen("Record.txt", "w");   //initialize pointer for the input file
+
+    if(recordPtr != NULL)
+    {
+        fprintf(recordPtr, "                 Amount       Date   Location\n");
+        for(int i = 0; i < numTransactions; i++)
+        {
+           fprintf(recordPtr, "Transaction %2d:", (i + 1));
+           fprintf(recordPtr, "  $%5d", record[i].amount);                 //print amount to file
+           fprintf(recordPtr, "%11s", record[i].date);                   //print amount to file
+           fprintf(recordPtr, "%11s\n", record[i].location);               //print amount to file
+        }
+    }
+    else
+    {
+        printf("Could not write to file");         //if we could not find the file pointer
+    }
+    fclose(recordPtr);              //close the file
+}
